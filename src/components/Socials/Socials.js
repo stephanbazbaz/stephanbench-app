20d48dab90dbdabbaaf3ddfabbbcfa031ce1eb37
stephanbazbaz <stephan@pwm.co.il>
2022-10-14 12:04:32 +0300

    fixes

--- src/components/Socials/Socials.js
+++ src/components/Socials/Socials.js
@@ -10,6 +10,12 @@ const Socials = () => {
           </div>)}
         <div className='dVLQAC'></div>
       </div>
+      <div className='mail-to'>
+        <div>
+          <a arget='_blank' rel="noreferrer" href='mailto:stephanbncml@gmail.com'>stephanbncml@gmail.com</a>
+        </div>
+        <div className='dVLQAC'></div>
+      </div>
     </div>
   )
 }
